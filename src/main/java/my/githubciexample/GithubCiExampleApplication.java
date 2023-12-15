92d6e49a894c2ace5a2c72030a7984e86d6393a7
JinDalgi <dalgi5986@gmail.com>
2023-12-15 14:12:15 +0900

    Initial commit

---
+++ src/main/java/my/githubciexample/GithubCiExampleApplication.java
@@ -0,0 +1,13 @@
+package my.githubciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/githubciexample/GithubCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package my.githubciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
